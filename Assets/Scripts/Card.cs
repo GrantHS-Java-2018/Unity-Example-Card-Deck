2ff5065289152adf65251c200ca10bcf369095f2
Nick Fenger <nfenger@pps.net>
2019-03-05 13:44:28 -0800

    Added Complete Unity Project

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+
+    public int suit;
+    public int rank; 
+    
+    // Start is called before the first frame update
+    
+    //Lookie here my dude it doesnt say start instead i replaced it with something else
+    void Awake()
+    {
+        Debug.Log("My card was made");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
